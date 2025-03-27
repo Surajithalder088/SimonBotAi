91550ce9852406bc2dea5bab19cae0684bdef86a
Surajit <surajithalder088@gmail.com>
2025-03-27 16:53:39 +0530

    m

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   typescript:{
     ignoreBuildErrors:true
-  }
+  },
+  experimental:{
+    
+  },
+  webpack:(config)=>{
+    return config
+  },
   /* config options here */
 };
 
